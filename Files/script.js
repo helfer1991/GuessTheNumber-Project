78c8d2bd98238fa4083e179df4d00123020f85b9
Helder Ferreira <35483115+helfer1991@users.noreply.github.com>
2021-04-19 18:02:54 +0100

    Update script.js

--- Files/script.js
+++ Files/script.js
@@ -20,7 +20,7 @@ document.querySelector('.check').addEventListener('click', () => {
     // When player wins
   } else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = ' Correct Number!';
-    displayMessage('🎉 Correct Number!');
+    displayMessage(' Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
